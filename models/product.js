8a757388a013d3ddce8410f706b2c16d7abc44ed
it27427 <istiak.tushar.it@gmail.com>
2024-03-28 22:45:57 +0600

    Storing Data in Files Via the Model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,22 @@
-const products = [];
+const { error } = require('console');
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (error, fileContent) => {
+    if (error) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +24,15 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
